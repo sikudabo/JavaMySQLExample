e784f732851cc6701489e19e0f30b4dcc241489f
Simeon Ikudabo <32518831+sikudabo@users.noreply.github.com>
2018-08-16 06:43:18 -0400

    SQL File that stores knowledge

---
+++ knowledgeBase.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE knowledgeBase;
+
+CREATE TABLE entityMap(
+
+entity VARCHAR(255),
+
+abstract TEXT,
+);
+
+Insert into entityMap(entity, abstract)
+
+VALUES("Simeon Ikudabo", "The creator of this app");
